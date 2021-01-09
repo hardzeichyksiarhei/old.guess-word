ed8980cd7c54d692796d52dc3bb737764aea34c8
hardzeichyksiarhei <hardzeichyksiarhei@gmail.com>
2021-01-09 06:40:12 +0300

    feat: add change avatar & name

--- src/components/players/PlayerItem.jsx
+++ src/components/players/PlayerItem.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 
 import * as playerActions from "../../store/actions/players";
@@ -6,19 +6,27 @@ import * as playerActions from "../../store/actions/players";
 import edit from "../../assets/edit.svg";
 
 const PlayerItem = (props) => {
+  const [isEditName, setIsEditName] = useState(false);
+
   const dispatch = useDispatch();
 
   const handleClickDeletePlayer = (player) => {
     dispatch(playerActions.deletePlayer(player.id));
   };
 
-  const handleClickUpdatePlayer = (player) => {
+  const handleClickUpdateAvatarPlayer = (player) => {
     dispatch(
       playerActions.updatePlayer(player.id, {
-        name: "Update player name",
         avatar:
           "https://images.unsplash.com/photo-1517423738875-5ce310acd3da?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=705&q=80",
       })
+    );
+  };
+
+  const handleClickUpdateNamePlayer = (event, player) => {
+    const name = event.target.value;
+    dispatch(
+      playerActions.updatePlayer(player.id, { name })
     );
   };
 
@@ -27,12 +35,18 @@ const PlayerItem = (props) => {
       <img
         className="setup-players-list-item-photo"
         src={props.player.avatar}
+        onClick={() => handleClickUpdateAvatarPlayer(props.player)}
         alt=""
       />
-      <input type="text" value={props.player.name} readOnly />
+      <input
+        type="text"
+        value={props.player.name}
+        readOnly={!isEditName}
+        onChange={(event) => handleClickUpdateNamePlayer(event, props.player)}
+      />
       <button
         className="setup-players-list-item-edit-button"
-        onClick={() => handleClickUpdatePlayer(props.player)}
+        onClick={() => setIsEditName(!isEditName)}
       >
         <img src={edit} alt="logo" />
       </button>
