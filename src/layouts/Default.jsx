0b29f0cca4c92227140df6ed8b7a897f89904fbb
hardzeichyksiarhei <hardzeichyksiarhei@gmail.com>
2021-01-10 04:22:53 +0300

    feat: add suspense loader

--- src/layouts/Default.jsx
+++ src/layouts/Default.jsx
@@ -1,6 +1,12 @@
 import React, { Suspense } from 'react'
 import { Outlet } from 'react-router-dom'
 
-const Default = () => <div className="default-layout"><Suspense fallback={null}><Outlet /></Suspense></div>
+import { Spin } from 'antd'
+
+const Default = () => <div className="default-layout">
+    <Suspense fallback={<Spin size="large" />}>
+        <Outlet />
+    </Suspense>
+</div>
 
 export default Default
